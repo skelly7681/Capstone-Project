55b96846a7124ad0f1b6928bc26915a186687a95
Dylan Leahy <leahy.dc@gmail.com>
2021-12-06 10:02:13 -0500

    DB refining

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -2,6 +2,9 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS seq_user_id;
+DROP TABLE IF EXISTS invites;
+DROP TABLE IF EXISTS invite_restaurant;
+DROP TABLE IF EXISTS restaurants;
 
 CREATE SEQUENCE seq_user_id
   INCREMENT BY 1
@@ -18,36 +21,47 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE invites (
+        invite_id serial NOT NULL,
+        sender_user_id integer NOT NULL,
+        closing_date date,
+        closing_time time,
+        unique_link varchar (200),
+        CONSTRAINT pk_invite_invite_id PRIMARY KEY (invite_id)
+);
+
+CREATE TABLE restaurants (
+        restaurant_id serial NOT NULL,
+        restaurant_name varchar(200),
+        restaurant_address varchar(200),
+        open_time time,
+        closing_time time,
+        phone_number varchar (20),
+        thumbnail_img varchar (200),
+        star_rating integer,
+        take_out boolean,
+        delivery boolean,
+        CONSTRAINT pk_restaurant_restaurant_id PRIMARY KEY (restaurant_id)
+);
+
+CREATE TABLE invite_restaurant (
+        invite_id integer,
+        restaurant_id integer,
+        vetoed boolean
+);
+
+
+ALTER TABLE invite_restaurant
+ADD FOREIGN KEY (invite_id)
+REFERENCES invite(invite_id);
+
+ALTER TABLE invite_restaurant
+ADD FOREIGN KEY (restaurant_id)
+REFERENCES restaurant(restaurant_id);
+
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+ROLLBACK TRANSACTION;
 
 COMMIT TRANSACTION;
-
-Start Transaction;
-
-Create table invites(
-invite_id serial NOT NULL,
-sender_user_id integer NOT NULL,
-closing_date date,
-closing_time time,
-unique_link varchar (200));
-
-create table invite_restaurant(
-invite_id integer,
-restaurant_id integer,
-vetoed boolean));
-
-create table restaurants(
-restaurant_id serial NOT NULL,
-restaurant_name varchar(200),
-restaurant_address varchar(200),
-open_time time,
-closing_time time,
-phone_number varchar (20),
-thumbnail_img varchar (200),
-star_rating integer,
-take_out boolean,
-delivery boolean));
-
-rollback transaction;
