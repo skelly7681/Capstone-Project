92bf996c6ffb6ca9fa4b48a1330c8ed633b3f633
Sam Whalen <samxwhalen@gmail.com>
2021-12-07 14:29:04 -0500

    Save front end progress

---
+++ vue/src/services/RestaurantService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// export default {
+
+//   getRestaurantByLocation(location) {
+//     return axios.get('/login', user)
+//   },
+
+//   register(user) {
+//     return axios.post('/register', user)
+//   }
+
+// }
