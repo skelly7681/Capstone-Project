7019ddff5d64fb67fdba4d789147bdcb646ffe36
sean <skelly7681@gmail.com>
2021-12-07 14:49:21 -0500

    controller start

--- java/src/main/java/com/techelevator/controller/TenderController.java
+++ java/src/main/java/com/techelevator/controller/TenderController.java
@@ -1,6 +1,15 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.*;
+import com.techelevator.model.Restaurant;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.List;
+
+@RestController
 
 public class TenderController {
 
@@ -15,4 +24,13 @@ public class TenderController {
         this.inviteDao = inviteDao;
 
     }
+
+    //Get All Restaurants by invite
+    @RequestMapping(path = "/restaurants", method = RequestMethod.GET)
+    public List<Restaurant> getAllRestaurantsById(int restaurantId) {
+        return restaurantDao.getRestaurantById(restaurantId);
+    }
+
+
+
 }
--- java/src/main/java/com/techelevator/dao/RestaurantDao.java
+++ java/src/main/java/com/techelevator/dao/RestaurantDao.java
@@ -14,4 +14,6 @@ public interface RestaurantDao {
 
     void thumbsDown(int restaurantId);
 
+    void thumbsUp(int restaurantId);
+
 }
