51c5133e1384d37793ee44d4141669eaa5b14581
Kevin Taylor <kevintaylor88@gmail.com>
2021-12-08 13:43:55 -0500

    Some of the API service started

--- java/src/main/java/com/techelevator/Service/RestaurantService.java
+++ java/src/main/java/com/techelevator/Service/RestaurantService.java
@@ -2,26 +2,86 @@ package com.techelevator.Service;
 
 import com.techelevator.dao.JdbcRestaurantDao;
 import com.techelevator.model.Restaurant;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-public class RestaurantService {
+@Component
+public class RestaurantService implements RestaurantServices {
 
-    public RestaurantService(JdbcRestaurantDao restaurantDao) {
-//        this.restaurantDao = restaurantDao;
+    private final String BASE_URL = "https://api.yelp.com/v3";
+    private final String API_KEY = "Pitw0I9VsxYc-wHu3Us_WTiMcfSehewIJw-sqDWBDdNdb5-haGIXthYN6oXI1enVAD3a-I0-r0tc1HdttbghiOMjEXDyaZnP7aC9M2lZXNryad-0AQsI0vz-m4-vYXYx";
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public Restaurant getRestaurantId() {
+        Restaurant restaurant = restTemplate.getForObject(BASE_URL, Restaurant.class); //this will need to include API-KEY
+        return restaurant;
     }
 
-    //where we call the yelp API
-
-    public Restaurant getRestaurant(){
-
-
-        return null;
-
+    public Restaurant getRestaurantName(){
+        Restaurant restaurantName = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantName;
     }
 
+    public Restaurant getRestaurantType(){
+        Restaurant restaurantType = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantType;
+    }
 
+    public Restaurant getRestaurantAddress(){
+        Restaurant restaurantAddress = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantAddress;
+    }
 
+    public Restaurant getOpenTime(){
+        Restaurant restaurantOpenTime = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantOpenTime;
+    }
 
+    public Restaurant getCloseTime(){
+        Restaurant restaurantCloseTime = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantCloseTime;
+    }
 
+    public Restaurant getPhoneNumber(){
+        Restaurant restaurantPhoneNumber = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantPhoneNumber;
+    }
 
+    public Restaurant getThumbnailImage(){
+        Restaurant restaurantThumbnailImage = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantThumbnailImage;
+    }
+
+    public Restaurant getStarRating(){
+        Restaurant restaurantStarRating = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantStarRating;
+    }
+
+    public Restaurant isTakeOut(){
+        Restaurant restaurantIsTakeOut = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantIsTakeOut;
+    }
+
+    public Restaurant isDelivery(){
+        Restaurant restaurantIsDelivery = restTemplate.getForObject(BASE_URL, Restaurant.class);
+        return restaurantIsDelivery;
+    }
+
+//    public Restaurant isVetoed(){
+//        Restaurant restaurantIsDelivery = restTemplate.getForObject(BASE_URL, Restaurant.class);
+//        return restaurantIsDelivery;
+//    }                                 --> would this be here since it is something the user sets?
 
 }
+
+
+
+//    public RestaurantService(JdbcRestaurantDao restaurantDao) {
+////        this.restaurantDao = restaurantDao;
+//    }
+//    //where we call the yelp API
+//    public Restaurant getRestaurant(){
+//        return null;
+//    }
+
+
