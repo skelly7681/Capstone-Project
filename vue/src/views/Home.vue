9337e0b88b32aea8d2ba8b885de2084f0507e8d7
Kevin Taylor <kevintaylor88@gmail.com>
2021-12-09 11:02:18 -0500

    Login Button Toggle Functioning

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home" id="home">
-    <router-link to="/login">Log In</router-link>&nbsp;|&nbsp;
+    <router-link to="/login" v-if="this.$store.state.currentUser">Log In</router-link>&nbsp;&nbsp;
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
