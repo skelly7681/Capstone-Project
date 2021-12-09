053f218495b49aedd88afe418a609ab6558febaa
sean <skelly7681@gmail.com>
2021-12-09 09:05:07 -0500

    css

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="home" id="home">
     <router-link to="/login">Log In</router-link>&nbsp;|&nbsp;
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,40 +1,53 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
-    </form>
-  </div>
+  <body class="container">
+    <div class="Master-row">
+            <div class="row-3">
+        <!-- insert and image here  -->
+        <img src="" alt="IMAGE HERE">
+      </div>
+      <div class="row-1">
+        <!-- insert an image here -->
+        <img src="" alt="IMAGE HERE">
+      </div>
+      <div id="login" class="row-2">
+      
+        <form class="form-signin" @submit.prevent="login">
+          <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
+          <div
+            class="alert_alert"
+            role="alert"
+            v-if="invalidCredentials"
+          >Invalid username and password!</div>
+          <div
+            class="alert_alert"
+            role="alert"
+            v-if="this.$route.query.registration"
+          >Thank you for registering, please sign in.</div>
+          <label for="username" class="sr-only">Username</label>
+          <input
+            type="text"
+            id="username"
+            class="form-control"
+            placeholder="Username"
+            v-model="user.username"
+            required
+            autofocus
+          />
+          <label for="password" class="sr-only">Password</label>
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="user.password"
+            required
+          />
+          <router-link :to="{ name: 'register' }">Need an account?</router-link>
+          <button type="submit" id="sign-in" >Sign in</button>
+        </form>
+      </div>
+    </div>
+  </body>
 </template>
 
 <script>
@@ -77,3 +90,39 @@ export default {
   }
 };
 </script>
+<style>
+body{
+  background-color: plum;
+}
+.Master-row{
+  
+  height: 650px;
+  border: solid;
+  border-width: 10px;
+  display: flex;
+
+}
+
+.row-1{
+  order: 1;
+  flex-basis: 33%;
+  border: solid;
+  border-width: 10px;
+}
+
+.row-2{
+  order: 2;
+  flex-basis: 33%;
+  border: solid;
+  border-width: 10px;
+}
+
+.row-3{
+  order: 3;
+  flex-basis: 33%;
+  border: solid;
+  border-width: 10px;
+}
+
+
+</style>
