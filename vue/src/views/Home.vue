7c2b965636ae90d8c236710abf24d2b7a2a6be45
Sam Whalen <samxwhalen@gmail.com>
2021-12-10 18:12:50 -0500

    Micro progress on the FE

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
+
   <div class="home" id="home">
     <router-link to="/login" v-if="this.$store.state.currentUser">Log In</router-link>&nbsp;&nbsp;
-    <h1>Home</h1>
+    <h1 class="pagename">Home</h1>
     <p>You must be authenticated to see this</p>
 
     <DummyRestaurant />
 
     <div id="viewRestaurants">
-      <div id="isLoading"> 
-        add gif img link here if isLoading is true.  Will display while resturants load
+
+      <div class="loading" v-if="isLoading">
+        <img src="../assets/loading.gif" alt="HELP!!!">
       </div>
+
       <div>
         This is where resturant cards will be displayed?  Do we need to pull from the store or will these populate based on
         the search results?
@@ -30,9 +33,26 @@ import DummyRestaurant from '@/components/DummyRestaurant.vue';
 
 export default {
   name: "home",
+    data() {
+    return {
+      Restaurants: [],
+      isLoading: true
+    };
+  },
+
   components: {
     DummyRestaurant
   }
 }
 
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+ .pagename {
+   font-family: 'pacifico';
+  }
+
+
+</style>
