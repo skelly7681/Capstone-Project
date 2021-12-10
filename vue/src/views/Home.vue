3519faa54abe4926c57c94e4545c4026bd6aaf90
Kevin Taylor <kevintaylor88@gmail.com>
2021-12-10 14:56:42 -0500

    some minor adds

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -3,7 +3,19 @@
     <router-link to="/login" v-if="this.$store.state.currentUser">Log In</router-link>&nbsp;&nbsp;
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+
     <DummyRestaurant />
+
+    <div id="viewRestaurants">
+      <div id="isLoading"> 
+        add gif img link here if isLoading is true.  Will display while resturants load
+      </div>
+      <div>
+        This is where resturant cards will be displayed?  Do we need to pull from the store or will these populate based on
+        the search results?
+      </div>
+      
+    </div>
 
 
 
