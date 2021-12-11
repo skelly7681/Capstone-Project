d8a082365da1a1493e136980051363629a30990a
Dylan Leahy <leahy.dc@gmail.com>
2021-12-11 15:54:02 -0500

    Changes

---
+++ vue/src/components/RestaurantList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class ="restaurant-container">
+      <restaurant-card v-bind:restaurant="restaurant" v-for="restaurant in $store.state.restaurants" v-bind:key="restaurant.id" />
+  </div>
+</template>
+
+<script>
+import RestaurantCard from './RestaurantCard.vue';
+
+export default {
+  components: { 
+      RestaurantCard 
+  },
+}
+</script>
+
+<style>
+.restaurant-container {
+    display:flex;
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+}
+</style>
