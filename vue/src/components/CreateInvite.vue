3a46464615da7a43a6ad1a20c5134247e16da54e
Kevin Taylor <kevintaylor88@gmail.com>
2021-12-13 12:12:55 -0500

    Removed submit ID from create Invite - not needed

--- vue/src/components/CreateInvite.vue
+++ vue/src/components/CreateInvite.vue
@@ -1,10 +1,6 @@
 // this will be the form to enter info needed to generate an invite - it will populate the home view
 <template>
 <div class="view-content">
-
-
-
-    
     <div class="invitation">
         <div class="nav">
             <router-link v-bind:to="{ name: 'home' }" id="home-button"> home </router-link>&nbsp;
@@ -17,10 +13,6 @@
                 <input type="date" id="dueDate" name="dueDate" v-model="invite.closingDate"/> 
                 <input type="time" id="dueTime" name="dueTime" v-model="invite.closingTime"/>
             </div>
-            <div>
-                <label for="userId">User ID: Enter number to track your Invite</label>
-                <input type="text" name="userId" id="userId" v-model="invite.senderUserId">
-            </div>
         </form>
 
         <!-- hide this until invite dates are picked -->
@@ -30,7 +22,6 @@
 
         <!-- all imported componets here -->
     
-
     </div>
 </div>
 </template>
